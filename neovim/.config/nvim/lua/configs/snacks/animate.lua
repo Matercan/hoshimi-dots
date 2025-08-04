e0ecd7277da4a80e90f3fabbde8ecc8020ac6562
Matercan <172917897+Matercan@users.noreply.github.com>
2025-08-04 20:40:47 +0200

    no more neovim

--- neovim/.config/nvim/lua/configs/snacks/animate.lua
+++
@@ -1,6 +0,0 @@
-return {
-  ---@type snacks.animate.Duration|number
-  duration = 20, -- ms per step
-  easing = "quadratic",
-  fps = 60,      -- frames per second. Global setting for all animations
-}

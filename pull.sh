d9d26a0cf5793c05c437a9cbcf1162d577add7eb
Matercan <172917897+Matercan@users.noreply.github.com>
2025-08-06 00:01:17 +0200

    Installation script updated for vesktop

--- pull.sh
+++ pull.sh
@@ -1,6 +1,8 @@
- DOTFILES_SOURCE="$(dirname "$(readlink -f "$0")")"
+#!/bin/bash
+
+DOTFILES_SOURCE="$(dirname "$(readlink -f "$0")")"
 USER_CONFIGS_DIRECTORY="${XDG_CONFIG_HOME:-$HOME/.config}"
-DOTFILES=("fastfetch" "fish" "ghostty" "hypr" "gtk-4.0" "vesktop/themes" "vesktop/settings" "wofi" "waybar" "eww")
+DOTFILES=("fastfetch" "fish" "ghostty" "hypr" "gtk-4.0" "vesktop/settings" "vesktop/themes" "wofi" "waybar" "eww")
 
 echo "This will delete all of the dotfiles here currently, and will replace the ones soruced with your dotfiles."
 
@@ -11,11 +13,26 @@ case "$choice" in
   * ) echo "Invalid response, aborting"; exit 1;;
 esac
 
-for dotfile in ${DOTFILES}
+for dotfile in "${DOTFILES[@]}"
 do
   echo "Copying ${USER_CONFIGS_DIRECTORY}/${dotfile}"
-  rm "${DOTFILES_SOURCE}/${dotfile}"
-  cp "${USER_CONFIGS_DIRECTORY}/${dotfile}" ${DOTFILES_SOURCE}
+  rm -rf "${DOTFILES_SOURCE}/${dotfile}"
+
+  if [ "$dotfile" = "vesktop/themes" ]; then
+    mkdir -p "${DOTFILES_SOURCE}/vesktop/themes"
+    cp -r ~/.config/vesktop/themes/* "${DOTFILES_SOURCE}/vesktop/themes"
+    continue
+fi
+if [ "$dotfile" = "vesktop/settings" ]; then
+    mkdir -p "${DOTFILES_SOURCE}/vesktop/settings"
+    cp -r ~/.config/vesktop/settings/* "${DOTFILES_SOURCE}/vesktop/settings"
+    continue
+fi
+
+  cp -r "${USER_CONFIGS_DIRECTORY}/${dotfile}" "${DOTFILES_SOURCE}"
 done
-rm "${DOTFILES_SOURCE}/starship/*"
+
+# Handle starship separately as it's a file, not a directory
+rm -rf "${DOTFILES_SOURCE}/starship/"
+mkdir "$DOTFILES_SOURCE/starship/"
 cp "${USER_CONFIGS_DIRECTORY}/starship.toml" "${DOTFILES_SOURCE}/starship/"

1a5429fe7bac36a71a318482d30f17c89762cc6c
Matercan <172917897+Matercan@users.noreply.github.com>
2025-08-11 18:37:40 +0200

    uhwuhwuhaduw

--- hypr/scripts/focus_vesktop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script focuses the Vesktop window.
-# Replace 'Vesktop' with the actual window class if different.
-hyprctl dispatch focuswindow class:vesktop
--- scripts/env.hpp
+++ scripts/env.hpp
@@ -1,23 +1,34 @@
 #pragma once
 
 #include "palette.hpp"
+#include <cstdlib>
 #include <string.h>
 #include <string>
 
 class EnvManager {
 public:
-  void setVariables(const std::string &wallpaperPath, int theme) {
+  void setVariables(const std::string &wallpaperPath,
+                    const std::string &assetsDir, int theme) {
     // Create the "KEY=VALUE" string using std::string concatenation
     std::string *wallpaperEnv = new std::string;
     *wallpaperEnv = "WALLPAPER=" + wallpaperPath;
 
-    char *ptr;
     char *envString = new char[wallpaperEnv->length() + 1];
     strcpy(envString, wallpaperEnv->c_str());
 
     // set the environment variable
     putenv(envString);
     delete wallpaperEnv;
+
+    std::string *assetsEnv = new std::string("ASSETS_DIR=" + assetsDir);
+
+    char *ptr = envString;
+    envString = new char[assetsEnv->length() + 1];
+    delete ptr;
+    strcpy(envString, assetsEnv->c_str());
+
+    putenv(envString);
+    delete assetsEnv;
 
     std::string *themeEnv = new std::string("HYPR_THEME=");
 

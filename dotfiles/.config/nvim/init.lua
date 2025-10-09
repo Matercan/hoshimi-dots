922340ab8dadd190da00488dbdea9102d4fea03c
matercan <matercan@proton.me>
2025-10-09 14:47:40 +0200

    Fix nvim: can read configs.options again

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -37,6 +37,6 @@ require("lazy").setup(plugins)
 
 
 -- Global options after plugins are loaded (e.g., configurations that might depend on plugins)
-require("lua.configs.options")
+require("configs.options")
 
 vim.cmd("Catppuccin latte")
